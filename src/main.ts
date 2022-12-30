f4c52bd1d84053ae66716a9e10bf6439570bbf10
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-12-30 13:34:18 +0900

    feat: main.ts 4000번 서버 오픈 22-12-30 01:34

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
 }
 bootstrap();
